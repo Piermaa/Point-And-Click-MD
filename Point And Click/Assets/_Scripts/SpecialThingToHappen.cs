949c6ee0e339a9041526aa813e38fc50a37e72cf
Juan Fangano <juanyfanga@gmail.com>
2023-04-26 01:25:42 -0300

    Level 2 avance y mejoras Level 1

---
+++ Point And Click/Assets/_Scripts/SpecialThingToHappen.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpecialThingToHappen : MonoBehaviour
+{
+    [SerializeField] InteractionManager interactionManager;
+
+    [Header ("Special Objects to Appear")]
+    [SerializeField] private List <GameObject> specialButtons = new List<GameObject>();
+
+
+    [SerializeField] private GameObject wallButton;
+    private Image buttonImage;
+
+    [Header("Sprites")]
+    [SerializeField] private Sprite wallSprite;
+    [SerializeField] private Sprite screamerImage;
+    [SerializeField] private Sprite blackOutImage;
+
+    private void Awake()
+    {
+        foreach (var objects in specialButtons)
+        {
+            objects.SetActive(false);
+        }
+
+        wallButton.SetActive(true);
+        buttonImage = wallButton.GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        if (interactionManager.textCollectionIndex == 14)
+        {
+            buttonImage.sprite = blackOutImage;
+        }
+
+        if (interactionManager.textCollectionIndex == 13)
+        {
+            buttonImage.sprite = screamerImage;
+        }
+
+        //buttonImage.sprite = wallSprite;
+
+        if (interactionManager.textCollectionIndex == 8)
+        {
+            specialButtons[0].SetActive(false);
+            specialButtons[1].SetActive(true);
+        }
+
+        if (interactionManager.textCollectionIndex == 7)
+        {
+            specialButtons[0].SetActive(true);
+        }
+    }
+
+
+}
