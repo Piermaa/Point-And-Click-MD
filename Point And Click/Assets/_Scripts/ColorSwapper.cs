061b8d2c6a46ca34f677715e4efdc0eed1dd8223
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2023-04-26 14:55:03 -0300

    color esguaper

---
+++ Point And Click/Assets/_Scripts/ColorSwapper.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//QUE CHUSMEAS GIL!!!1!!
+[System.Serializable]
+public class Colors
+{
+    [Tooltip("POR ESTE COLOR ME LLAMAS EN INTERACTIONMANAGER.CS")]public string colorName;
+    public Color color;
+}
+public class ColorSwapper : MonoBehaviour
+{
+    public List<Colors> colors = new(1);
+    public Dictionary<string,Colors> colorsDic = new();
+    private Button butt;
+    private Image img;
+    private void Awake()
+    {
+        foreach (Colors col in colors)
+        {
+            colorsDic.Add(col.colorName, col);
+        }
+        img = GetComponent<Image>();
+        butt = GetComponent<Button>();
+    }
+
+    public void SwapColor(string colorName)
+    {
+       img.color = colorsDic[colorName].color;
+    }
+}
