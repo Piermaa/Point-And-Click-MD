0c5d0e5b1f589bf8f67bcd8ba98c1d0e749447ca
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2023-04-27 14:59:41 -0300

    parte uno

--- Point And Click/Assets/_Scripts/ScaleChanger.cs
+++ Point And Click/Assets/_Scripts/ScaleChanger.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class ScaleChanger : MonoBehaviour
 {
     [SerializeField]
-    private Vector3 targetScale;
+    private Vector3 targetScale1;
+
+    [SerializeField]
+    private Vector3 targetScale2;
 
     private Vector3 originalScale;
     private void Awake()
@@ -13,10 +16,20 @@ public class ScaleChanger : MonoBehaviour
         originalScale = transform.localScale;
     }
 
-    public void ChangeScale()
+    public void ChangeScale(int num)
     {
-        transform.localScale = targetScale;
+        switch (num)
+        {
+            case 1:
+             transform.localScale = targetScale1;
+                break;
+            case 2:
+                transform.localScale = targetScale2;
+                break;
+        }
     }
+       
+    
     public void DefaultScale()
     {
         transform.localScale = originalScale;
